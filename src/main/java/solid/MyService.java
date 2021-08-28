e14ce490e92926ab89b7515d5c9db5be582eb393
jassada.sir <jassada.sir@ntb.co.th>
2021-08-28 18:21:19 +0700

    solid myservice test

--- src/main/java/solid/MyService.java
+++ src/main/java/solid/MyService.java
@@ -3,10 +3,15 @@ package solid;
 import java.util.Random;
 
 public class MyService {
+    Random random;
+
     public String generateUUID() {
         String date = "DEMO";
-        Random random = new Random();
         int number = random.nextInt(10);
         return date + number;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
     }
 }
--- src/test/java/solid/MyServiceTest.java
+++ src/test/java/solid/MyServiceTest.java
@@ -1,7 +1,8 @@
 package solid;
 
-
 import org.junit.jupiter.api.Test;
+
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,7 +10,16 @@ class MyServiceTest {
 
     @Test
     void generateUUID() {
+        Random random = new Random7();
         MyService myService = new MyService();
+        myService.setRandom(random);
         assertEquals("DEMO7", myService.generateUUID());
+    }
+}
+
+class Random7 extends Random {
+    @Override
+    public int nextInt(int bound) {
+        return  7;
     }
 }
